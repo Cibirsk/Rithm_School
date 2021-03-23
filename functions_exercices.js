abb291d2951330a55c963d8ead9e4d61ea561370
Cibirsk <stvl@gmx.com>
2021-03-23 12:22:36 +0100

    Update functions_exercices.js

--- functions_exercices.js
+++ functions_exercices.js
@@ -23,6 +23,44 @@ console.log(printDay(2));
 //Last Element
 //this function takes in one parameter (an array) and returns the last value in the array.
 function lastElement(arr){
-    return arr[length -1];
+    return arr[arr.length -1];
 }
-console.log([1,2,3,4]);
+console.log(lastElement([1,2,3,4,5]));
+
+//Number Compare
+//this function takes in two parameters (both numbers). If the first is greater than the second, 
+//this function returns "First is greater". If the second number is greater than the first, 
+//the function returns "Second is greater". Otherwise the function returns "Numbers are equal"
+function numberCompare(a,b){
+    if(a>b){
+        return "a is greater than b";
+    }else if(a<b){
+        return "b is greater than a";
+    }else{
+        return "numbers are equal";
+    }
+}
+console.log(numberCompare(7,7));
+
+//singleLetterCount
+//this function takes in two parameters (two strings). 
+//The first parameter should be a word and the second should be a letter. 
+//The function returns the number of times that letter appears in the word. 
+//The function should be case insensitive (does not matter if the input is lowercase or uppercase). 
+//If the letter is not found in the word, the function should return 0
+function singleLetterCount(word,x){
+    let count=0;
+    for(let i=0;i<word.length;i++){
+        if(word[i].toLowerCase() === x.toLowerCase()){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(singleLetterCount("testT","T"));
+
+
+//PART 2
+//multipleLetterCount
+//this function takes in one parameter (a string) and returns an object with
+//the keys being the letters and the values being the count of the letter
