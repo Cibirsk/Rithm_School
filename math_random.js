133563d84d65361f0305fbe7168b1802c586a4fc
Cibirsk <stvl@gmx.com>
2021-03-24 16:29:57 +0100

    none

--- math_random.js
+++
@@ -1,7 +0,0 @@
-// utilisation de Math.random pour créer un chiffre aléatoire entre 0 et 1
-let a = Math.random();
-if(a < 0.5){
-    console.log(`inférieur à 0.5, le chiffre est: ${a}`);
-}else{
-    console.log(`supérieur à 0.5, le chiffre est: ${a}`);
-}
