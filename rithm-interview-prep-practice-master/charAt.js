60d998bcad615c2abe216cbab7d17f7b872e3b46
Cibirsk <stvl@gmx.com>
2021-03-24 17:47:35 +0100

    none

--- rithm-interview-prep-practice-master/charAt.js
+++ rithm-interview-prep-practice-master/charAt.js
@@ -1,4 +1,9 @@
 // add whatever parameters you deem necessary - good luck!
-function charAt(){
-
+function charAt(str,number){
+    if(number > str.length){
+        return "";
+    }else{
+        return str[number];
+    }
 }
+console.log(charAt("awesome", 12));
