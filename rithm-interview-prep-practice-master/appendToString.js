a6fa496e3bc2358d4ab6c222e5e9468425245a71
Cibirsk <stvl@gmx.com>
2021-03-24 17:33:44 +0100

    none

---
+++ rithm-interview-prep-practice-master/appendToString.js
@@ -0,0 +1,5 @@
+// add whatever parameters you deem necessary - good luck!
+function appendToString(strA,strB){
+    return strA.concat(strB);
+}
+console.log(appendToString("Hello", " World!"));
---
+++ rithm-interview-prep-practice-master/prependToString.js
@@ -0,0 +1,5 @@
+// add whatever parameters you deem necessary - good luck!
+function prependToString(strA,strB){
+    return strB.concat(strA);
+}
+console.log(prependToString("world", "hello "));
