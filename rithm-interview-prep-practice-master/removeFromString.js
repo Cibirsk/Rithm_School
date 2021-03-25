edfcf7bdd8ec01036899f9f791649a9b4d7b6b52
Cibirsk <stvl@gmx.com>
2021-03-25 18:52:08 +0100

    none

--- rithm-interview-prep-practice-master/removeFromString.js
+++ rithm-interview-prep-practice-master/removeFromString.js
@@ -1,4 +1,7 @@
 // add whatever parameters you deem necessary - good luck!
-function removeFromString(){
-
+function removeFromString(str,index,char){
+    let newStr;
+    newStr=str.slice((index - char),char);
+    return newStr;
 }
+console.log(removeFromString("Elie",0,1));
