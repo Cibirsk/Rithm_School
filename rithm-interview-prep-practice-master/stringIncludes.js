3e7a08c481b45d40b2fcf5952f5f560d4f6eccf4
Cibirsk <stvl@gmx.com>
2021-03-25 11:59:22 +0100

    none

--- rithm-interview-prep-practice-master/stringIncludes.js
+++ rithm-interview-prep-practice-master/stringIncludes.js
@@ -1,4 +1,11 @@
 // add whatever parameters you deem necessary - good luck!
-function stringIncludes(){
-
+function stringIncludes(str,letter){
+    //for(let i=0;i<str.length;i++){
+    for(let i in str){
+        if(str[i] === letter){
+            return true;
+        }
+    }
+    return false;
 }
+console.log(stringIncludes("awesome","e"));
--- rithm-interview-prep-practice-master/stringIndexOf.js
+++ rithm-interview-prep-practice-master/stringIndexOf.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function stringIndexOf(){
-
+function stringIndexOf(str,letter){
+    for(let i=0;i<str.length;i++){
+        if(str[i]===letter){
+            return i;
+        }
+    }
+    return -1;
 }
+console.log(stringIndexOf("awesome","e"));
