f81bbe0e6d589d633a16bf9ed663ee05e9c55356
Cibirsk <stvl@gmx.com>
2021-03-27 10:12:14 +0100

    none

--- rithm-interview-prep-practice-master/includes.js
+++ rithm-interview-prep-practice-master/includes.js
@@ -7,6 +7,15 @@ function includes(collection, value, index){
             }
         }
         return false;
+    }else if(index !== undefined){
+        for(let i=index; i<collection.length;i++){
+            if(collection[i] === value){
+                return true;
+            }
+        }
+        return false;
     }
 }
-console.log(includes([1,2,3], 1));
+console.log(includes([1,2,3], 2,0));
+console.log(includes("abcd","a",1));
+console.log(includes({ 1:"a", 2:"b", 3:"c" }, "a"));
--- rithm-interview-prep-practice-master/indexOf.js
+++ rithm-interview-prep-practice-master/indexOf.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function indexOf(){
-
+function indexOf(arr, value){
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]=== value){
+            return i;
+        }    
+    }
+    return -1;
 }
+
+let my_arr=[5,10,15,20];
+console.log(indexOf(my_arr, 20));
