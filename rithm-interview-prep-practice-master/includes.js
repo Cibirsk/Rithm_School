3f4e482a80d7ba13cf66e3144512b42b15a2c526
Cibirsk <stvl@gmx.com>
2021-03-27 09:33:31 +0100

    Update includes.js

--- rithm-interview-prep-practice-master/includes.js
+++ rithm-interview-prep-practice-master/includes.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function includes(){
-
+function includes(collection, value, index){
+    if(index === undefined){
+        for(let i=0; i<collection.length; i++){
+            if(collection[i] === value){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+console.log(includes([1,2,3], 1));
