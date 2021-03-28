49dca9d7daa2c5716e97fa17e638c1656b55c6f4
Cibirsk <stvl@gmx.com>
2021-03-28 11:05:36 +0200

    Update entries.js

--- rithm-interview-prep-practice-master/entries.js
+++ rithm-interview-prep-practice-master/entries.js
@@ -2,3 +2,14 @@
 function entries(){
 
 }
+
+let obj11 = { a: 1, b: 2, c: 3 };
+console.log(entries(obj11));
+// [["a",1], ["b",2], ["c",3]]
+
+let obj22 = { first: 'Matt', last: 'Lane', isDogOwner: true };
+console.log(entries(obj22));
+// [["first","Matt"], ["last","Lane"], ["isDogOwner",true]]
+
+let obj44 = {};
+console.log(entries(obj44)); // []
