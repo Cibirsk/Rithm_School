7af410aa848936071d030e08f18449cfc469f92f
Cibirsk <stvl@gmx.com>
2021-03-29 12:50:05 +0200

    Update twoHighest.js

--- rithm-interview-prep-practice-master/twoHighest.js
+++ rithm-interview-prep-practice-master/twoHighest.js
@@ -1,6 +1,5 @@
 // add whatever parameters you deem necessary - good luck!
 function twoHighest(arr){
-    /*
     let myArrResult=[0,0];
 
     for(let i=0;i<arr.length;i++){
@@ -17,23 +16,18 @@ function twoHighest(arr){
     }
 
     return myArrResult;
-    */
     let myArr1=arr;
     let myArr2=arr;
     for(let i=0;i<myArr1;i++){
-        if(arr[i] < arr[i+1]){
-            myArr1.splice(i+1,1);
+        if(myArr1[i] < myArr1[i+1]){
+            myArr1.splice(i,1);
         }
     }
     return myArr1;
 }
-
 
 console.log(twoHighest([1, 2, 10, 8])); // [8, 10]
 console.log(twoHighest([6, 1, 9, 10, 4])); // [9,10]
 console.log(twoHighest([4, 25, 3, 20, 19, 5])); // [20,25]
 console.log(twoHighest([1, 2, 2])); // [2, 2];
 
-let testArr=[1,2,10,8];
-testArr.splice(0,1);
-console.log(testArr);
