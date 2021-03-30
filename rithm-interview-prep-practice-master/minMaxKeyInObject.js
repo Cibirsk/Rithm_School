9cd3090be4f87c655f630405ad371ce533d09df8
Cibirsk <stvl@gmx.com>
2021-03-30 11:11:23 +0200

    none

--- rithm-interview-prep-practice-master/minMaxKeyInObject.js
+++ rithm-interview-prep-practice-master/minMaxKeyInObject.js
@@ -1,4 +1,13 @@
 // add whatever parameters you deem necessary - good luck!
-function minMaxKeyInObject(){
+function minMaxKeyInObject(obj){
 
 }
+
+console.log(minMaxKeyInObject({ 2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e' }));
+// [1, 10]
+console.log(minMaxKeyInObject({ 1: 'Elie', 4: 'Matt', 2: 'Tim' }));
+// [1, 4]
+
+let a="1";
+let b="2";
+console.log(parseInt(a,10)+parseInt(b,10));
