9072ce42400564c13992d83b66ba42d815685e06
Cibirsk <stvl@gmx.com>
2021-03-30 11:30:16 +0200

    none

--- rithm-interview-prep-practice-master/stringFromObject.js
+++ rithm-interview-prep-practice-master/stringFromObject.js
@@ -2,3 +2,13 @@
 function stringFromObject(){
 
 }
+
+console.log(stringFromObject({ a: 1, b: '2' }));
+// "a = 1, b = 2"
+
+
+console.log(stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false }));
+// "name = Elie, job = Instructor, isCatOwner = false"
+
+
+stringFromObject({}); // ""
