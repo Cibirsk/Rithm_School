19944dc3d73697a77ea3958e9d17d4567f940132
Cibirsk <stvl@gmx.com>
2021-03-31 11:27:36 +0200

    none

--- rithm-interview-prep-practice-master/countNumbers.js
+++ rithm-interview-prep-practice-master/countNumbers.js
@@ -2,3 +2,9 @@
 function countNumbers(){
 
 }
+
+console.log(countNumbers(['a','b','3','awesome','4'])); // 2
+console.log(countNumbers(['32', '55', 'awesome', 'test', '100'])); // 3
+console.log(countNumbers(`[]`)); // 0
+console.log(countNumbers(['4','1','0','NaN'])); // 3
+console.log(countNumbers(['7', '12', 'a', '', '6', '8', ' '])); // 4
