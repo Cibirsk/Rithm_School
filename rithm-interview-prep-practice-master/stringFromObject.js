165618022f04d1ccf2f134cb1db920e232365e6c
Cibirsk <stvl@gmx.com>
2021-03-31 11:23:12 +0200

    Update stringFromObject.js

--- rithm-interview-prep-practice-master/stringFromObject.js
+++ rithm-interview-prep-practice-master/stringFromObject.js
@@ -1,18 +1,19 @@
 // add whatever parameters you deem necessary - good luck!
 function stringFromObject(obj){
-    let resultString="";
+    let myArr=[];
     for(let i in obj){
-        resultString= resultString + i + " " + "=" + " " + obj[i] + "," + " ";
+        myArr.push(`${i} = ${obj[i]}`);
     }
-    return resultString;
+    return myArr.join(", ");
 }
+
+
+
 
 console.log(stringFromObject({ a: 1, b: '2' }));
 // "a = 1, b = 2"
 
-
 console.log(stringFromObject({ name: 'Elie', job: 'Instructor', isCatOwner: false }));
 // "name = Elie, job = Instructor, isCatOwner = false"
-
 
 console.log(stringFromObject({})); // ""
