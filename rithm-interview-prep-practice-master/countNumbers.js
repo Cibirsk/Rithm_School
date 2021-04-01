b3929f948f3c4901a131ab473130d1d389196f0e
Cibirsk <stvl@gmx.com>
2021-04-01 20:38:53 +0200

    none

--- rithm-interview-prep-practice-master/countNumbers.js
+++ rithm-interview-prep-practice-master/countNumbers.js
@@ -2,12 +2,11 @@
 function countNumbers(arr){
     let count=0;
     for(let i=0;i<arr.length;i++){
-       if(typeof arr[i] === 'string'){
-        count= count+1; 
+        if((parseInt(arr[i],10)+1) <= 0 || (parseInt(arr[i],10)+1) >= 0){
+        count++; 
        }
-        
     }
-    //return count;
+    return count;
 }
 
 console.log(countNumbers(['a','b','3','awesome','4'])); // 2
@@ -15,11 +14,3 @@ console.log(countNumbers(['32', '55', 'awesome', 'test', '100'])); // 3
 console.log(countNumbers(`[]`)); // 0
 console.log(countNumbers(['4','1','0','NaN'])); // 3
 console.log(countNumbers(['7', '12', 'a', '', '6', '8', ' '])); // 4
-
-let a= 'v';
-let b=1;
-a=parseInt(a);
-let c=a+b;
-
-console.log(c);
-console.log(typeof c);
--- rithm-interview-prep-practice-master/removeVowels.js
+++ rithm-interview-prep-practice-master/removeVowels.js
@@ -2,3 +2,9 @@
 function removeVowels(){
 
 }
+
+console.log(removeVowels("Hello!")); // "Hll!"
+console.log(removeVowels("Tomatoes")); // "Tmts"
+console.log(removeVowels("Reverse Vowels In The String")); // "Rvrs Vwls n Th Strng"
+console.log(removeVowels("aeiou")); // ""
+console.log(removeVowels("why try, shy fly?")); // "why try, shy fly?"
