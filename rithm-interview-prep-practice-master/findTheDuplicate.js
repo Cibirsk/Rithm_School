06e752aaf6014f33dad6f791a39f728b2082363e
Cibirsk <stvl@gmx.com>
2021-04-02 18:46:56 +0200

    none

--- rithm-interview-prep-practice-master/findTheDuplicate.js
+++ rithm-interview-prep-practice-master/findTheDuplicate.js
@@ -1,4 +1,8 @@
 // add whatever parameters you deem necessary - good luck!
-function findTheDuplicate(){
-
+function findTheDuplicate(arr){
+    
 }
+
+console.log(findTheDuplicate([1,2,1,4,3,12])); // 1
+console.log(findTheDuplicate([6,1,9,5,3,4,9])); // 9
+console.log(findTheDuplicate([2,1,3,4])); // undefined
