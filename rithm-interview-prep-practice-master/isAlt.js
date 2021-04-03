c2d069919d2c960fad5d65bca027a902783c67b9
Cibirsk <stvl@gmx.com>
2021-04-03 08:49:23 +0200

    none

--- rithm-interview-prep-practice-master/isAlt.js
+++ rithm-interview-prep-practice-master/isAlt.js
@@ -1,4 +1,8 @@
 // add whatever parameters you deem necessary - good luck!
-function isAlt(){
+function isAlt(str){
 
 }
+
+console.log(isAlt("amazon")); // true
+console.log(isAlt("apple")); // false
+console.log(isAlt("banana")); // true
