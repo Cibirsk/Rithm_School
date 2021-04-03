ba4a515ee647a6afdc87cc4cc008616edac90177
Cibirsk <stvl@gmx.com>
2021-04-03 17:38:31 +0200

    Update isAlt.js

--- rithm-interview-prep-practice-master/isAlt.js
+++ rithm-interview-prep-practice-master/isAlt.js
@@ -1,8 +1,21 @@
 // add whatever parameters you deem necessary - good luck!
 function isAlt(str){
-
+    let vovels="aeiou";
+    let consonants="bcdfghjklmnpqrstvwxyz";
+    let response;
+    for(let i=0;i<str.length; i++){
+        if(vovels.includes(str[i]) && consonants.includes(str[i+1])){
+            response=true;
+            i++;
+        }else if(consonants.includes(str[i]) && vovels.includes(str[i+1])){
+            response=true;
+            i++;
+        }
+    }
+    return response;
 }
 
-console.log(isAlt("amazon")); // true
-console.log(isAlt("apple")); // false
-console.log(isAlt("banana")); // true
+console.log(isAlt("abecid"));
+//console.log(isAlt("amazon")); // true
+//console.log(isAlt("apple")); // false
+//console.log(isAlt("banana")); // true
