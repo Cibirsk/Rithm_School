100a0758837bd76f20e131a4ebc15e0d8e33b8bb
Cibirsk <stvl@gmx.com>
2021-04-03 08:33:27 +0200

    none

--- rithm-interview-prep-practice-master/totalCaps.js
+++ rithm-interview-prep-practice-master/totalCaps.js
@@ -1,4 +1,16 @@
 // add whatever parameters you deem necessary - good luck!
-function totalCaps(){
-
+function totalCaps(arr){
+    let count=0;
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            if(arr[i][j] === arr[i][j].toUpperCase()){
+                count++;
+            }
+        }
+    }
+    return count;
 }
+
+console.log(totalCaps(["AwesomE", "ThIngs", "hAppEning", "HerE"])); // 8
+console.log(totalCaps(["Elie", "Matt", "Tim"])); // 3
+console.log(totalCaps(["hello", "world"])); // 0
