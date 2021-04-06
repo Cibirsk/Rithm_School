3edceb22f553382464d6e6a40be1c1333cc82db2
Cibirsk <stvl@gmx.com>
2021-04-06 17:49:35 +0200

    none

---
+++ interm_nested_objects.js
@@ -0,0 +1,24 @@
+let instructorData = {
+    name: "Elie",
+    additionalData: {
+        instructor: true,
+        favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
+        moreDetails: {
+            favoriteBasketballTeam: "New York Knicks",
+            numberOfSiblings: 3,
+            isYoungest: true,
+            hometown: {
+                city: "West Orange",
+                state: "NJ",
+            },
+            citiesLivedIn: ["Seattle", "Providence", "New York"]
+        }
+    }
+};
+
+instructorData.name; // "Elie"
+instructorData.additionalData.instructor; // true
+instructorData.additionalData.favoriteHobbies[2]; // "Coding"
+instructorData.additionalData.moreDetails.favoriteBasketballTeam; // "New York Knicks"
+instructorData.additionalData.moreDetails.hometown.state; // "NJ"
+instructorData.additionalData.moreDetails.citiesLivedIn[1]; // "Providence"
