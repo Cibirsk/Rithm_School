3f46af959982a242f5b0d68c2db1b595f757eb3b
Cibirsk <stvl@gmx.com>
2021-05-01 10:36:36 +0200

    none

--- rithm-interview 2/charAt.js
+++ rithm-interview 2/charAt.js
@@ -1,4 +1,10 @@
 // add whatever parameters you deem necessary - good luck!
-function charAt(){
-
+function charAt(word,index){
+    for(let i=0;i<word.length;i++){
+        if(i==index){
+            return word[i];
+        }
+    }
+    return '';
 }
+console.log(charAt("awesome",1));
